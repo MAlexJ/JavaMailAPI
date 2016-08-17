e5481fc1dad99a57b7d45f453dcc171bfb0c3f62
malexj <alexmaximovj@gmail.com>
2016-08-17 17:43:50 +0300

    fix

--- src/main/java/com/malex/sendingEmailWithAttachment/SendAttachmentInEmail.java
+++ src/main/java/com/malex/sendingEmailWithAttachment/SendAttachmentInEmail.java
@@ -42,12 +42,29 @@ import java.util.Properties;
  */
 public class SendAttachmentInEmail {
 
-    // Assuming you are sending email through relay.jangosmtp.net
+    // Assuming you are sending email
     private static final String HOST_EMAIL = "smtp.gmail.com";
-    private static final String SMTP_AUTH = "true";
-    private static final String SMTP_PORT = "587";
-    private static final String START_TLS = "true";
+    private static final String SMTP_AUTH_EMAIL = "true";
+    private static final String SMTP_PORT_EMAIL = "587";
+    private static final String START_TLS_EMAIL = "true";
 
+    // Properties setting email
+    private static final String HOST = "mail.smtp.host";
+    private static final String SMTP_AUTH = "mail.smtp.auth";
+    private static final String SMTP_PORT = "mail.smtp.port";
+    private static final String START_TLS = "mail.smtp.starttls.enable";
+
+    // Properties email
+    private static final Properties properties;
+
+    // Initialization Properties email
+    static {
+        properties = new Properties();
+        properties.put(SMTP_AUTH, SMTP_AUTH_EMAIL);
+        properties.put(START_TLS, START_TLS_EMAIL);
+        properties.put(HOST, HOST_EMAIL);
+        properties.put(SMTP_PORT, SMTP_PORT_EMAIL);
+    }
 
     public static void main(String[] args) {
 
@@ -62,26 +79,20 @@ public class SendAttachmentInEmail {
         String fileName = "log/iPOS_0_0.log";
 
         send(sender, userEmail, passwordEmail, recipient, subjectMessage, fileName);
-
     }
 
     static void send(String sender, final String userEmail, final String passwordEmail, String recipient, String subjectMessage, String pathFile) {
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", SMTP_AUTH);
-        props.put("mail.smtp.starttls.enable", START_TLS);
-        props.put("mail.smtp.host", HOST_EMAIL);
-        props.put("mail.smtp.port", SMTP_PORT);
 
-        // Get the Session object.
-        Session session = Session.getInstance(props,
+        // #########  Step #1: Get the Session object. ##########
+        Session session = Session.getInstance(properties,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
                         return new PasswordAuthentication(userEmail, passwordEmail);
                     }
                 });
 
+        // #########  Step #2:  Create a default MimeMessage object. #########
         try {
-            // Create a default MimeMessage object.
             Message message = new MimeMessage(session);
 
             // Set From: header field of the header.
@@ -120,13 +131,13 @@ public class SendAttachmentInEmail {
             // Send the complete message parts
             message.setContent(multipart);
 
-            // Send message
+            //  #########  Step #3: Send message #########
             Transport.send(message);
 
-            System.out.println("Sent message successfully....");
-
         } catch (MessagingException e) {
-            e.printStackTrace();
+            System.err.println(" >>> The message is not sent !!! <<< ");
         }
+
+        System.out.println(" >>> Sent message successfully <<< ");
     }
 }
